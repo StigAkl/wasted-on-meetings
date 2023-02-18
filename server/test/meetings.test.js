3857be9302d0720f5b7f3fb00e5b91337dbcaf97
stigakl <stigakl@outlook.com>
2023-02-18 17:41:30 +0100

    make tests greeeeeeen

--- server/test/meetings.test.js
+++ server/test/meetings.test.js
@@ -1,12 +1,13 @@
 const request = require("supertest");
 const app = require("../app");
 const database = require("../data/Repositories/MeetingsRepository");
-const meetingsUrl = "/api/v1/meetings/1";
 jest.mock("../data/Repositories/MeetingsRepository");
 
 let mockGetMeetings = jest.spyOn(database, "getMeetings");
 
 describe("When calling meeting endpoints", () => {
+  const meetingsUrl = "/api/v1/meeting/1";
+
   beforeEach(() => {
     jest.clearAllMocks();
   });
