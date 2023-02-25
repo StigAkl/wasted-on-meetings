b7a87c83d1d1d37c6a4f6d2de32c9e47d401d194
stigakl <stigakl@outlook.com>
2023-02-26 00:52:16 +0100

    target aws

--- client/src/constants/api.ts
+++ client/src/constants/api.ts
@@ -1,4 +1,5 @@
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+//export const API_BASE_URL = import.meta.env.VITE_APP_BASE_URL_AWS;
 export const authUrl = `${API_BASE_URL}/api/v1/auth/signin`;
 export const refreshUrl = `${API_BASE_URL}/api/v1/auth/refresh`;
 export const signUpUrl = `${API_BASE_URL}/api/v1/auth/signup`;
