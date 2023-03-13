ad9794d2d2697a3d8f674dc776fb247693bf535f
stigakl <stigakl@outlook.com>
2023-03-13 23:49:49 +0100

    not going serverless anymore

--- server/index.js
+++ server/index.js
@@ -1,9 +1,4 @@
 const app = require("./app");
 const port = process.env.PORT || 5000;
-const serverless = require("serverless-http");
 
-if (process.env.NODE_ENV === "development") {
-  app.listen(port, () => console.log("Server listening on port " + port));
-} else {
-  module.exports.handler = serverless(app);
-}
+app.listen(port, () => console.log("Server listening on port " + port));
