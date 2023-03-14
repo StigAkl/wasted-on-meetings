7c18efa3ea7a6ba1ee768582058e95c85788335f
stigakl <stigakl@outlook.com>
2023-03-15 00:31:45 +0100

    turn on logging

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 //app.use(bodyParser.urlencoded({ extended: true }));
 app.enable("trust proxy");
-//app.use(morgan("combined"));
+app.use(morgan("combined"));
 app.use(cors);
 app.use(commonResponse);
 
